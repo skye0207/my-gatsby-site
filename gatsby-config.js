9f3a2be99339faf653fc3fdc3224a39e94f2e207
skye <gwang@taosdata.com>
2022-02-08 20:20:48 +0800

    use plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `My Gatsby Site`,
+      title: `My Gatsby Si`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
      <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://resource.nreal.ai/web/images/air/air_kv.jpg"
+      />
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://resource.nreal.ai/web/images/air/light_kv.jpg"
+      />
     </Layout>
   )
 }
